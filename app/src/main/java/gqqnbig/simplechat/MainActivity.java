b3878a69f62501a43336e660a619f0851d8f6ecc
gqqnbig <gqqnb2005@gmail.com>
2016-08-06 16:42:10 -0700

    初始提交

--- app/src/main/java/gqqnbig/simplechat/MainActivity.java
+++ app/src/main/java/gqqnbig/simplechat/MainActivity.java
@@ -8,20 +8,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView idTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idTextView=(TextView) findViewById(R.id.idTextView);
+        idTextView.setText("Gqqnbig");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
